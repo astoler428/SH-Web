c3d8a6607fd791419bcc0ec9807f23593822c391
Ari Stoler <astoler428@gmail.com>
2024-03-19 18:40:46 -0500

    role responsiveness for any width

--- src/components/RoleDialog.js
+++ src/components/RoleDialog.js
@@ -71,11 +71,11 @@ export default function RoleDialog({ thisPlayer, game, roleOpen, setRoleOpen, se
     >
       <DialogContent sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
         {game.settings.type === GameType.MIXED_ROLES && (
-          <Box sx={{ width: { xs: 140, sm: 200 }, maxWidth: { xs: `calc(100vh/3)`, sm: "100%" }, marginRight: { xs: 2, sm: 4 } }}>
+          <Box sx={{ width: { xs: 140, sm: 200 }, maxWidth: { xs: `calc(100vh/3)` }, marginRight: { xs: 2, sm: 4 } }}>
             <img src={teamImg} draggable="false" style={{ width: "100%" }} />
           </Box>
         )}
-        <Box sx={{ width: { xs: 140, sm: 200 }, maxWidth: { xs: `calc(100vh/3)`, sm: "100%" }, position: "relative" }}>
+        <Box sx={{ width: { xs: 140, sm: 200 }, maxWidth: { xs: `calc(100vh/3)` }, position: "relative" }}>
           <img src={roleImg} draggable="false" style={{ width: "100%", visibility: "hidden" }} /> {/* just a placeholder to for the sizing */}
           <img
             src={roleImg}
