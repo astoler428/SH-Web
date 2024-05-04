65f942a81c9ef70ff43b7664d9de9fbe33f9ca61
Ari Stoler <astoler428@gmail.com>
2024-05-04 13:43:34 -0500

    try again

--- src/pages/Lobby.js
+++ src/pages/Lobby.js
@@ -83,7 +83,9 @@ export default function Lobby({ name, game, setGame, isConnected, setError }) {
   }
 
   useEffect(() => {
-    setCurrentSettings(game?.settings);
+    if (game?.settings) {
+      setCurrentSettings(game?.settings);
+    }
   }, [game?.settings]);
 
   const players = game?.players?.map((player, idx) => (
