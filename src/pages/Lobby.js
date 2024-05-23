4fa1151d5cac45d10a6bbcb21010ff0ae3f5b791
Ari Stoler <astoler428@gmail.com>
2024-05-22 21:20:37 -0500

    rm console log

--- src/pages/Lobby.js
+++ src/pages/Lobby.js
@@ -73,7 +73,6 @@ export default function Lobby({ name, game, setGame, isConnected, setError }) {
   }
 
   async function handleSettingsChange(propName, propValue) {
-    console.log(propName, propValue);
     if (game.host === name) {
       propValue = propName === GameSettings.TYPE ? propValue : !game.settings[propName];
       setCurrentSettings(prevSettings => ({ ...prevSettings, [propName]: propValue }));
